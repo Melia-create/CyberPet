09045cc7fa0b58369a1bf520937858af9fb63522
Amelia Riding <ariding@csl.local>
2021-02-11 13:29:25 +0000

    made more changes to pets js

--- pets.js
+++ pets.js
@@ -11,55 +11,52 @@ class CyberPet {
 
     eat() {
         if (this.hunger > 4) {
-            console.log(this.name, "is enjoying the food you've given them");
             this.happiness = this.happiness + 5;
             this.energy = this.energy + 5;
             this.hunger = this.hunger - 5;
-            return this;
+            return `${this.name} devoured the food!`
         } else {
-            console.log(this.name, "is full!")
-            return this;
+            return`${this.name} is full!`
         }
     }
 
     drink() {
         if (this.thirst > 4) {
-            console.log(this.name, "is lapping up the water!");
             this.happiness = this.happiness + 5;
             this.energy = this.energy + 5;
             this.thirst = this.thirst - 5;
-            return this;
+            return `${this.name} is lapping up the water!`
         } else {
-            console.log(this.name, "'s thirst is quenched!")
-            return this;
+            return `${this.name}'s thirst is quenched!`
+        }
+
+
+    }
+
+    sleep(){
+        if (this.isSleeping){
+        this.energy = this.energy + 10;
+        this.hunger = this.hunger + 3;
+        this.thirst = this.thirst + 3;
+        return `${this.name} is currently sleeping!`
+        } else 
+        return `${this.name} is awake and wants to play!`
+    }
+
+    play(){
+        if (this.bored > 4 & this.energy > 4){
+            this.bored = this.bored - 5;
+            this.energy = this.energy - 5;
+            this.thirst = this.thirst + 5
+            this.hunger = this.hunger + 5
+            return `${this.name} is playing.`
+        } else {
+            return `${this.name} is tired from all the playing!`
         }
     }
 
-    play() {
-        if (this.bored > 4 & this.energy > 4) {
-            console.log(this.name, "is playing and having fun!")
-        this.bored = this.bored - 5;
-        this.thirst = this.thirst + 5;
-        this.hunger = this.hunger + 5;
-        this.energy = this.energy - 4;
-        return this;
-    } else {
-        console.log(this.name, "'s tired from all the playing!")
-    }
-
-    sleep() {
-        if (this.isSleeping) {
-            console.log(this.name, "'s currently sleeping, don't disturb them!")
-            this.energy = this.energy + 10;
-            this.hunger = this.hunger + 3;
-            this.thirst = this.thirst + 3;
-            return this;
-        } else
-            console.log(this.name, "'s awake and wants to play!")
-    }
-
-    checkStatus() {
-        console.log("Name:", this.name, "Happiness:", this.happiness, "Energy", this.energy, "Hunger:", this.hunger, "Thirst:", this.thirst, "Boredom:", this.bored)
+    checkStatus(){
+        console.log ("Name:", this.name, "Happiness:", this.happiness, "Energy", this.energy, "Hunger:", this.hunger, "Thirst:", this.thirst, "Boredom:", this.bored)
     }
 }
 
